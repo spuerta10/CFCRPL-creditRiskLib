62f0208b1bee33ffa3dbececf9f30cc5f382a996
spuerta10 <puertasantiago2002@gmail.com>
2024-08-25 11:20:43 -0500

    feat: Added interface for all load steps to inherite from

---
+++ credit_risk_pipeline_lib/load/load_interface.py
@@ -0,0 +1,16 @@
+from abc import abstractmethod
+
+from credit_risk_pipeline_lib.pipeline_step_interface import PipelineStepInterface
+
+from pandas import DataFrame
+
+
+class LoadInterface(PipelineStepInterface):
+    @abstractmethod
+    def load(data: DataFrame):
+        """Loads given data to a certain source
+
+        Args:
+            data (DataFrame): Data to be loaded into a source.
+        """
+        pass
