3b2d8ae0d797cec5ef6fc69acbdfff5247637dba
Santiago Puerta <spuertaf@eafit.edu.co>
2024-08-25 00:05:06 -0500

    feat: Added CSV file ingestion class

---
+++ credit_risk_pipeline_lib/ingest/csv.py
@@ -0,0 +1,19 @@
+from credit_risk_pipeline_lib.ingest.ingest_interface import IngestInterface
+
+from pandas import DataFrame, read_csv
+
+class CSV(IngestInterface):
+    def __init__(
+            self,
+            path: str,
+            step_name: str = "CSV ingestion"
+    ):
+        # TODO: Validate the path
+        self._path = path
+        self._step_name = step_name
+    
+    def ingest(self) -> DataFrame:
+        try:
+            return read_csv(self._path)
+        except Exception as e:
+            ...
