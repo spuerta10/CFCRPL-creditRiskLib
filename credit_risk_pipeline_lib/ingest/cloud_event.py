b82f792bbf8b8b166d51de4678a8693b41127e1e
Santiago Puerta <spuertaf@eafit.edu.co>
2024-08-25 00:04:33 -0500

    feat: Added cloud event v1 and v2 ingestion classes

--- credit_risk_pipeline_lib/ingest/cloud_event.py
+++ credit_risk_pipeline_lib/ingest/cloud_event.py
@@ -1,8 +1,37 @@
+from base64 import b64decode
+
 from credit_risk_pipeline_lib.ingest.ingest_interface import IngestInterface
 
-class CloudEvent(IngestInterface):
+from cloudevents.http import CloudEvent
+from pandas import DataFrame
+
+
+class CloudEventV1(IngestInterface):
     def __init__(
             self,
-            cloud_event: ...
+            cloud_event: CloudEvent,
+            step_name: str = "Cloud Event v1 ingestion"
     ):
-        ...
+        self._cloud_event = cloud_event
+        self._step_name = step_name
+
+
+    def ingest(self) -> DataFrame:
+        """Ingests a Cloud Functions v1 event.
+
+        Returns:
+            DataFrame: The cloud event data transformed to DataFrame.
+        """
+        try:
+            pubsub_msg: dict = b64decode(self._cloud_event.data["message"]["data"]).decode()
+            ingested_data = DataFrame(pubsub_msg)
+            return ingested_data 
+        except Exception as e:
+            ...
+
+
+class CloudEventV2(IngestInterface):
+    """
+    Future implementation of ingestion of a V2 cloud event.
+    """
+    pass
