9a3dd2a37a6436ea67d50fef9d6a11bdb9b1dc5f
Santiago Puerta <spuertaf@eafit.edu.co>
2024-08-25 00:07:18 -0500

    refactor: Replaced relative import for absolute one

--- credit_risk_pipeline_lib/ingest/ingest_interface.py
+++ credit_risk_pipeline_lib/ingest/ingest_interface.py
@@ -1,11 +1,12 @@
 from abc import abstractmethod
 
-from pipeline_step_interface import PipelineStepInterface
+from credit_risk_pipeline_lib.pipeline_step_interface import PipelineStepInterface
 
 from pandas import DataFrame
 
 class IngestInterface(PipelineStepInterface):
 
+    @abstractmethod
     def ingest(self) -> DataFrame:
         """Ingest data from a source, returns the ingested data in Dataframe.
 
