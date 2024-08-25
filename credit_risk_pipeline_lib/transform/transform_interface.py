fcc56a172e1b149268481029db8d047bf38221d5
Santiago Puerta <spuertaf@eafit.edu.co>
2024-08-25 00:09:13 -0500

    feat: Added transform interface for transform steps to inherit from

---
+++ credit_risk_pipeline_lib/transform/transform_interface.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+
+from credit_risk_pipeline_lib.pipeline_step_interface import PipelineStepInterface
+
+from pandas import DataFrame
+
+class TransformInterface(PipelineStepInterface):
+    
+    @abstractmethod
+    def transform(self, data: DataFrame) -> DataFrame:
+        """_summary_
+
+        Args:
+            data (DataFrame): _description_
+
+        Returns:
+            DataFrame: _description_
+        """
+        pass
