adeb3c8b4fdf1e5ed9ff26892656560a56b10d9d
spuerta10 <puertasantiago2002@gmail.com>
2024-08-29 20:12:34 -0500

    feat(JSON): Added json data as ingestion source

---
+++ credit_risk_pipeline_lib/pipeline/ingest/json.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from os.path import abspath
+
+from credit_risk_pipeline_lib.pipeline.ingest.ingest_interface import IngestInterface
+
+from pandas import DataFrame, read_json
+
+
+class JSON(IngestInterface):
+    def __init__(self, content: str | dict):
+        if isinstance(content, str) and not Path(content).exists():
+            raise ValueError("")
+        self._content = content
+    
+    
+    def ingest(self) -> DataFrame:
+        # for now the JSON passed must be plain
+        try:
+            data: DataFrame = read_json(abspath(self._content), index=[0]) \
+                if isinstance(self._content, str) else DataFrame(self._content, index=[0])
+            return data
+        except Exception as e:
+            raise ValueError("")
+    
+    
