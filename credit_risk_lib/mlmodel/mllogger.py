4b3a3009331789b766b80be0a77532cfdbb1f025
spuerta10 <puertasantiago2002@gmail.com>
2024-09-12 16:41:52 -0500

    feat: Added class to log in mlflow params, artifacts and metrics

---
+++ credit_risk_lib/mlmodel/mllogger.py
@@ -0,0 +1,21 @@
+from credit_risk_lib.mlmodel.conf_validator import ConfValidator
+
+from pydantic import BaseModel
+import mlflow
+
+
+class MLLogger(ConfValidator):
+    def __init__(self, model_conf_path: str, model_conf_schema: None | BaseModel = None) -> None:
+        super.__init__(model_conf_path, model_conf_schema)
+        
+    
+    def log_param(self, *args):
+        mlflow.log_param(args)
+    
+    
+    def log_artifact(self, *args):
+        mlflow.log_artifact(args)
+        
+    
+    def log_metric(self, *args):
+        mlflow.log_metric(args)
